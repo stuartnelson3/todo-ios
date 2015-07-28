d1359b2109c2d23fbcca71d6211a271bc1c7560d
Stuart Nelson <stuartnelson3@gmail.com>
2015-07-28 23:28:50 +0200

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Stuart Nelson on 7/28/15.
+//  Copyright (c) 2015 Stuart Nelson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
